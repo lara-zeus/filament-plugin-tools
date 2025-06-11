f6df6fc7caf321bd01dce464f583c0a762689e79
Ash Monsh <wh7r.com@gmail.com>
2025-06-12 02:13:39 +0300

    add support for configurable enum

---
+++ src/Concerns/HasEnums.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace LaraZeus\FilamentPluginTools\Concerns;
+
+trait HasEnums
+{
+    protected array $enums = [];
+
+    public function enums(array $enums): static
+    {
+        $this->enums = $enums;
+
+        return $this;
+    }
+
+    public function getEnums(): array
+    {
+        return $this->enums;
+    }
+
+    public static function getEnum(string $enum): null | Enum
+    {
+        return array_merge(
+            config(static::get()->getId() . '.enums'),
+            (new static)::get()->getEnums()
+        )[$enum] ?? null;
+    }
+}
