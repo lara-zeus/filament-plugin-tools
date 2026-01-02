7df5a7f41aa83d46eccabf9b85189c566b407076
atmonshi <1952412+atmonshi@users.noreply.github.com>
2026-01-02 06:28:16 +0000

    Fix styling

--- src/Concerns/HasNavigationGroupLabel.php
+++ src/Concerns/HasNavigationGroupLabel.php
@@ -3,17 +3,18 @@
 namespace LaraZeus\FilamentPluginTools\Concerns;
 
 use Closure;
+use UnitEnum;
 
 trait HasNavigationGroupLabel
 {
-    public function navigationGroupLabel(string|\UnitEnum|null|Closure $label): static
+    public function navigationGroupLabel(string | UnitEnum | null | Closure $label): static
     {
         $this->navigationGroupLabel = $label;
 
         return $this;
     }
 
-    public function getNavigationGroupLabel(): string|\UnitEnum|null
+    public function getNavigationGroupLabel(): string | UnitEnum | null
     {
         return $this->evaluate($this->navigationGroupLabel);
     }
